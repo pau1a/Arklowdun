7bcd29f4f3a98ca2051270318c2dc39a3f29ee3f
Paula Livingstone <paula@paulalivingstone.com>
2025-09-02 18:26:00 +0100

    fix: use millisecond timestamps for soft-deletes

---
+++ migrations/202509021400_fk_integrity_check.sql
@@ -0,0 +1,5 @@
+-- id: 202509021400_fk_integrity_check
+-- checksum: 73bd694b82dade89a7ba76edd325f77750dc3393d61845f3f7a7315af3563d12
+-- Verify foreign key integrity after rebuild
+
+PRAGMA foreign_key_check;
