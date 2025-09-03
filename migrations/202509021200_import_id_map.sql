d3f291737438853bce71744ad7e3c41510cd2602
Paula Livingstone <paula@paulalivingstone.com>
2025-09-03 07:26:55 +0100

    chore(import): validate FKs before commit

--- migrations/202509021200_import_id_map.sql
+++ migrations/202509021200_import_id_map.sql
@@ -1,5 +1,5 @@
 -- id: 202509021200_import_id_map
--- checksum: 61d2f4b368e1b7c9ed08ce17f17de6b17ecc119c50c2d0a90e48232d4d535c35
+-- checksum: bc0d8f7c1b34bff4e922a4b7554e15e28ca3ec88fd1119dd67effb537a8940c8
 
 BEGIN;
 
@@ -7,10 +7,8 @@ CREATE TABLE IF NOT EXISTS import_id_map (
   entity TEXT NOT NULL,
   old_id TEXT NOT NULL,
   new_uuid TEXT NOT NULL,
-  PRIMARY KEY (entity, old_id)
+  PRIMARY KEY (entity, old_id),
+  UNIQUE (new_uuid)
 );
-
-CREATE UNIQUE INDEX IF NOT EXISTS import_id_map_new_uuid_idx
-  ON import_id_map(new_uuid);
 
 COMMIT;
